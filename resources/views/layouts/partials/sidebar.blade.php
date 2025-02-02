36ee9a8a6c6073fc9ef9829eb901a9084935082b
mohammad-taznim-azad <azadtaznim@gmail.com>
2025-02-03 00:59:16 +0600

    updated

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -3,32 +3,32 @@
     <div>
         <div class="logo-wrapper">
             <a href="{{ route('index') }}">
-                <img class="img-fluid for-light" src="{{ url(@$setting->logo) }}" style="height:80px" alt="">
-                <img class="img-fluid for-dark" src="{{ url(@$setting->logo) }}" style="height:80px" alt="">
+                <img class="img-fluid for-light" src="{{ url(@$setting->logoDark) }}" style="height:80px" alt="">
+                <img class="img-fluid for-dark" src="{{ url(@$setting->logoDark) }}" style="height:80px" alt="">
             </a>
             <div class="back-btn"><i class="fa fa-angle-left"></i></div>
             <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="grid"></i></div>
         </div>
         <div class="logo-icon-wrapper"><a href="{{ route('index') }}"><img class="img-fluid"
-                    src="{{url($setting->logo ?? $setting->logo)}}" style="height: 50px" alt=""></a></div>
+                    src="{{url($setting->logoDark ?? $setting->logoDark)}}" style="height: 50px" alt=""></a></div>
         <nav class="sidebar-main">
             <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
             <div id="sidebar-menu">
                 <ul class="sidebar-links" id="simple-bar">
                     <li class="back-btn"><a href="{{ route('index') }}"><img class="img-fluid"
-                                src="{{url($setting->logo ?? $setting->logo)}}" alt=""></a>
+                                src="{{url($setting->logoDark ?? $setting->logoDark)}}" alt=""></a>
                         <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2"
                                 aria-hidden="true"></i></div>
                     </li>
                     <li class="sidebar-main-title">
                         <div>
                             <h6 class="">Welcome!</h6>
-                            <p class="">Greetings from {{ @$setting->companyName }}</p>
+                            <p class="">Greetings from bKash</p>
                         </div>
                     </li>
                     <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav active"
                             href="{{ route('index') }}"><i data-feather="home"> </i><span>Dashboard</span></a>
-                    </li>               
+                    </li>              
                   
                     @canany(['survey.show','survey.complete_survey'])
                     <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#"><i data-feather="archive"></i><span>Mutual Evaluation</span></a>
