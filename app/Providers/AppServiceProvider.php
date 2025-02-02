00aa4fa6dd5f7efdcc6f08b62f16a97057485a5e
mohammad-taznim-azad <azadtaznim@gmail.com>
2025-02-02 23:51:22 +0600

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $setting = Setting::query()->first();
-         View::share(['setting' => $setting]);
+        View::share(['setting' => $setting]);
     }
 }
